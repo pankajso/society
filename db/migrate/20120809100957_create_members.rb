09a3f09745eb0fd1bc9f9bff3f731899b4741105
Pankaj Sonawane <pankajso@gmail.com>
2012-08-09 15:42:18 +0530

    added member model

---
+++ db/migrate/20120809100957_create_members.rb
@@ -0,0 +1,12 @@
+class CreateMembers < ActiveRecord::Migration
+  def change
+    create_table :members do |t|
+      t.integer, :refnum
+      t.string, :name
+      t.string, :contactno
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
