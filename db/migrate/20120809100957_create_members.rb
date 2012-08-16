e7bd70ecaddd7dffe547998705b0b9fe707066ca
Pankaj Sonawane <pankajso@gmail.com>
2012-08-16 17:08:58 +0530

    syntax error removed

--- db/migrate/20120809100957_create_members.rb
+++ db/migrate/20120809100957_create_members.rb
@@ -1,9 +1,9 @@
 class CreateMembers < ActiveRecord::Migration
   def change
     create_table :members do |t|
-      t.integer, :refnum
-      t.string, :name
-      t.string, :contactno
+      t.integer :refnum
+      t.string :name
+      t.string :contactno
       t.string :email
 
       t.timestamps
