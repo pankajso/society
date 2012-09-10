d0a33bcb7abbce805c9552c2eb54e6d98f496a43
Pankaj Sonawane <pankajso@gmail.com>
2012-09-10 17:31:13 +0530

    refnum changed to string

--- db/migrate/20120910115530_change_refno_to_string.rb
+++ db/migrate/20120910115530_change_refno_to_string.rb
@@ -1,8 +1,13 @@
 class ChangeRefnoToString < ActiveRecord::Migration
   def up
-    
+    change_table :members do |t|
+      t.change :refnum, :string
+    end
   end
 
   def down
+    change_table :members do |t|
+      t.change :refnum, :integer
+    end
   end
 end
