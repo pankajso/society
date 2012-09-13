e16b9acf8a767db0af5b302a831f089d5bce05b7
Pankaj Sonawane <pankajso@gmail.com>
2012-09-13 18:01:05 +0530

    development enviroment setup to use devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,6 +18,9 @@ Societymaintainence::Application.configure do
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
+  
+  #Devise Setup
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
