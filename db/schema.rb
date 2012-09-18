89b3b257e116343833c80a73166575f7ed2ec5fd
Pankaj Sonawane <pankajso@gmail.com>
2012-09-18 18:39:50 +0530

    Schema file update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120917125131) do
+ActiveRecord::Schema.define(:version => 20120918130726) do
 
   create_table "members", :force => true do |t|
     t.string   "refnum"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(:version => 20120917125131) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
