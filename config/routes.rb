e2436da7d498fa8528eb8276e6d2f0c58594fa12
Pankaj Sonawane <pankajso@gmail.com>
2012-09-19 19:58:36 +0530

    Restricted the access for Dash board

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Societymaintainence::Application.routes.draw do
+  authenticated :user do
+    root :to => 'home#index'
+  end
+  root :to => 'home#index'
   devise_for :users
 
-  get "home/index"
+  #get "home/index"
 
   resources :members
 
@@ -54,7 +58,7 @@ Societymaintainence::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
+  #root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
