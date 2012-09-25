3fb033fe65ed45469647ac512c186b2b55a49adb
Pankaj Sonawane <pankajso@gmail.com>
2012-09-25 17:51:29 +0530

    Initial position set

--- app/models/monthwise_status.rb
+++ app/models/monthwise_status.rb
@@ -1,3 +1,31 @@
+require 'state_machine'
 class MonthwiseStatus < ActiveRecord::Base
   attr_accessible :month, :position
+  state_machine :position, :initial => :idle do
+    event :generate do 
+      transition [:idle, :generated] => :generated
+    end
+    event :generated do
+      transition [:generated] => :approved
+    end
+    event :approved do
+      transition [:approved] => :close
+    end
+    position :idle, :generate, :generated, :approved, :close
+    
+    #Update the postion of the month
+    def self.update_position(month, position)
+      @month_position = MonthwiseStatus.find(:all, :conditions => ["month = ?", month]).first
+      if @month_position.position.blank?
+        @month_position.update_attribute(position, 'idle')
+        @month_position.save
+      end
+      if position == 'generated'
+        @month_position.generated!
+      elsif position == 'approved'
+        @month_position.approved!
+      elsif position == 'close'
+        @month_position.close!
+      end
+    end
 end
