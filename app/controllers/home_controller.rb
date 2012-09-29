9c8f0b93b5b34fcd7aa5add9194cc482266ee8d3
Pankaj Sonawane <pankajso@gmail.com>
2012-09-29 16:58:54 +0530

    Dashboard page displayed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
   
   def index
+    @monthly = MonthlyMaintainence.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @members }
+    end
   end
 end
